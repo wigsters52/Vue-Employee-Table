f228bc61548fb067df6c3116a57a5362baf08bc4
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-10-29 19:38:27 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" @edit:employee="editEmployee" />
+  </div>
+</template>
+
+<script>
+  import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      EmployeeTable,
+      EmployeeForm,
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Richard Hendricks',
+            email: 'richard@piedpiper.com',
+          },
+          {
+            id: 2,
+            name: 'Bertram Gilfoyle',
+            email: 'gilfoyle@piedpiper.com',
+          },
+          {
+            id: 3,
+            name: 'Dinesh Chugtai',
+            email: 'dinesh@piedpiper.com',
+          },
+        ],
+      }
+    },
+    methods: {
+      addEmployee(employee) {
+        const lastId =
+          this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+
+        this.employees = [...this.employees, newEmployee];
+      },
+      deleteEmployee(id) {
+        this.employees = this.employees.filter(
+        employee => employee.id !== id
+        )
+      },
+      editEmployee(id, updatedEmployee) {
+        this.employees = this.employees.map(employee =>
+          employee.id === id ? updatedEmployee : employee
+        )
+      }
+    }
+  }
+</script>
+
+<style>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+</style>
